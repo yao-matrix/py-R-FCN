88b2488e4d3bbb939f400ac299922ebea4a34e67
YAO Matrix <yaoweifeng0301@126.com>
2017-06-23 17:01:22 +0800

    add run environ settings
    Signed-off-by: YAO Matrix <yaoweifeng0301@126.com>

--- run.sh
+++ run.sh
@@ -3,14 +3,16 @@
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
 export GLOG_minloglevel=2
 
+core_num=`nproc`
 
 unset OMP_NUM_THREADS
-# export OMP_NUM_THREADS=44
-# export MKL_NUM_THREADS
+export OMP_NUM_THREADS=${core_num}
+export MKL_NUM_THREADS=${core_num}
+# export OMP_DYNAMIC="False"
 
 # export OMP_WAIT_POLICY=passive
 unset MKL_THREADING_LAYER
 # export MKL_THREADING_LAYER=gnu
-
+export KMP_AFFINITY=compact,1,0,granularity=fine
 
 ./experiments/scripts/rfcn_end2end.sh 0 ResNet-101 pascal_voc
